66bceee57e2c8d57d16f9d942faab62d28b83e3a
Kashif Ali <kashifali874@gmail.com>
2018-10-24 16:20:55 -0700

    Changing the views based on choices

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -44,15 +44,26 @@ class ViewController: UIViewController {
         
         
         // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
+        storyTextView.text = story1
+        topButton.setTitle(answer1a, for: .normal)
+        bottomButton.setTitle(answer1b, for : .normal)
         
     }
 
     
     // User presses one of the buttons
     @IBAction func buttonPressed(_ sender: UIButton) {
-    
+        print("\(sender.tag)")
         // TODO Step 4: Write an IF-Statement to update the views
-                
+        if sender.tag == 1 {
+            storyTextView.text = story3
+            topButton.setTitle(answer3a, for: .normal)
+            bottomButton.setTitle(answer3b, for : .normal)
+        } else if sender.tag == 2 {
+            storyTextView.text = story2
+            topButton.setTitle(answer2a, for: .normal)
+            bottomButton.setTitle(answer2b, for : .normal)
+        }
         // TODO Step 6: Modify the IF-Statement to complete the story
         
     
